a89a9ac6cb9833a89ef0a5710317d7a6fe193959
Yonatan Shiri <yonatanshiri95@gmail.com>
2022-03-07 10:53:47 +0200

    commonMethods+managePages+pages

---
+++ main/pages/TestProjectFormPage.cs
@@ -0,0 +1,89 @@
+﻿using CSharpProjectTemplate.main.Base;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpProjectTemplate.main.pages
+{
+
+    /**
+     * This is an Example for Page Object class.
+     */
+    class TestProjectFormPage:BasePage
+    {
+        public TestProjectFormPage(IWebDriver driver):base(driver)
+        {
+            this.driver = driver;
+        }
+
+        // Elements Locators.
+        private By countries = By.XPath(PropertyReader.readElementsRepoItem("tp_country_dropdown_xpath"));
+        private By address = By.XPath(PropertyReader.readElementsRepoItem("tp_address_input_xpath"));
+        private By email = By.XPath(PropertyReader.readElementsRepoItem("tp_email_input_xpath"));
+        private By phone = By.XPath(PropertyReader.readElementsRepoItem("tp_phone_input_xpath"));
+        private By saveButton = By.XPath(PropertyReader.readElementsRepoItem("tp_save_button_xpath"));
+        private By confirmMessage = By.XPath(PropertyReader.readElementsRepoItem("tp_confirm_message_xpath"));
+
+
+        /**
+         * This method return the country input element.
+         *
+         * @return WebElement
+         */
+        public IWebElement getCountries()
+        {
+            return driver.FindElement(countries);
+        }
+
+        /**
+         * This method return the address input element.
+         *
+         * @return WebElement
+         */
+        public IWebElement getAddressElement()
+        {
+            return driver.FindElement(address);
+        }
+
+        /**
+         * This method return the email input element.
+         *
+         * @return WebElement
+         */
+        public IWebElement getEmailElement()
+        {
+            return driver.FindElement(email);
+        }
+
+        /**
+         * This method return the phone input element.
+         *
+         * @return WebElement
+         */
+        public IWebElement getPhoneElement()
+        {
+            return driver.FindElement(phone);
+        }
+
+        /**
+         * This method return the saveButton button element.
+         *
+         * @return WebElement
+         */
+        public IWebElement getSaveButtonElement()
+        {
+            return driver.FindElement(saveButton);
+        }
+
+        /**
+         * This method return the confirmMessage element.
+         *
+         * @return WebElement
+         */
+        public IWebElement getConfirmMessageElement()
+        {
+            return driver.FindElement(confirmMessage);
+        }
+    }
+}
---
+++ main/pages/TestProjectLoginPage.cs
@@ -0,0 +1,67 @@
+﻿using CSharpProjectTemplate.main.Base;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpProjectTemplate.main.pages
+{
+    /**
+    * This is an Example for Page Object class.
+    */
+    class TestProjectLoginPage : BasePage
+    {
+
+        public TestProjectLoginPage(IWebDriver driver) :base(driver)
+        {
+            this.driver = driver;
+        }
+
+        // Elements Locators.
+        private By username = By.XPath(PropertyReader.readElementsRepoItem("tp_user_name_input_xpath"));
+        private By password = By.XPath(PropertyReader.readElementsRepoItem("tp_password_input_xpath"));
+        private By loginButton = By.XPath(PropertyReader.readElementsRepoItem("tp_login_button_xpath"));
+        private By logoutButton = By.XPath(PropertyReader.readElementsRepoItem("tp_logout_button_xpath"));
+
+        /**
+         * This method return the username input element.
+         *
+         * @return WebElement.
+         */
+        public IWebElement getUserNameElement()
+        {
+            return driver.FindElement(username);
+        }
+
+        /**
+         * This method return the password input element.
+         *
+         * @return WebElement.
+         */
+        public IWebElement getPasswordElement()
+        {
+            return driver.FindElement(password);
+        }
+
+        /**
+         * This method return the login button element.
+         *
+         * @return WebElement.
+         */
+        public IWebElement getLoginBtnElement()
+        {
+            return driver.FindElement(loginButton);
+        }
+
+        /**
+         * This method return the logout button element.
+         *
+         * @return WebElement.
+         */
+        public IWebElement getLogOutBtnElement()
+        {
+            return driver.FindElement(logoutButton);
+        }
+
+    }
+}
